192aecd2ea8cda55092f1822d22034084f1ff081
SEG <segdev.mx@gmail.com>
2023-05-13 18:18:12 -0500

    feat: add a run function with the main loop in lib.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::{
     fmt::Arguments,
     io::{Result, Write},
     process,
+    time::{Duration, Instant},
 };
 
 use crossterm::{
@@ -10,12 +11,44 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
+use emulator::Emulator;
+use event_handler::handle_event;
+
 pub mod config;
 pub mod emulator;
+pub mod event_handler;
 pub mod opcode;
 pub mod opcodes;
 pub mod ram;
 pub mod screen;
+
+pub fn run<W>(mut emulator: Emulator, output: &mut W) -> Result<()>
+where
+    W: Write,
+{
+    let mut last_tick = Instant::now();
+
+    loop {
+        let timeout = emulator
+            .config
+            .tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::ZERO);
+
+        if last_tick.elapsed() >= emulator.config.tick_rate {
+            emulator.tick(output)?;
+            last_tick = Instant::now();
+        }
+
+        if handle_event(timeout, &mut emulator, output)? {
+            break;
+        }
+    }
+
+    restore(output)?;
+
+    Ok(())
+}
 
 pub fn quit<W>(output: &mut W, err: Arguments) -> !
 where
